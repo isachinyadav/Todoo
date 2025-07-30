db0e87496d9966cc1002524dc9a9fe810a9105c0
Sachin Yadav <sachinyadav68763@gmail.com>
2025-07-31 02:59:17 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
      <Router>
       <Routes>
-        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='/Home' element={<TodoPage />} />
       </Routes>
